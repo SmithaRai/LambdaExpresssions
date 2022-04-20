f9fb0ba9ded70fb24294a23f0e2e434f29177d34
unknown <SmithaRai.com>
2022-04-20 16:42:41 +0530

    Added : added println Statement

---
+++ src/lambdaexpression/MathCalculation.java
@@ -0,0 +1,18 @@
+package lambdaexpression;
+
+import java.util.Scanner;
+
+interface math
+   {
+	void calculate(int a , int b);
+   }
+public class MathCalculation {
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("******************MATH CALCULATIONS USING LAMBDA**************************");
+	}
+
+}
