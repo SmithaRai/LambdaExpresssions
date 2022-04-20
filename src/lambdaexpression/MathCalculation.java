6d594201beb2b5bfdba40c455444f35025a3dbb0
unknown <SmithaRai.com>
2022-04-20 16:57:21 +0530

    Added : added math calculations

--- src/lambdaexpression/MathCalculation.java
+++ src/lambdaexpression/MathCalculation.java
@@ -13,6 +13,14 @@ public class MathCalculation {
 		// TODO Auto-generated method stub
 		
 		System.out.println("******************MATH CALCULATIONS USING LAMBDA**************************");
+     
+		math add = (a,b)->System.out.println("addition : "+(a+b));
+		math subtract = (a,b)->System.out.println("subtraction : "+(a-b));
+		math division = (a,b)->System.out.println("division : "+(a/b));
+		
+		add.calculate(3, 4);
+		subtract.calculate(10, 5);
+		division.calculate(50, 5);
 	}
 
 }
