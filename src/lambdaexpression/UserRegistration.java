628dce899c14457e296f221ecdede7375156fc82
unknown <SmithaRai.com>
2022-04-23 20:43:03 +0530

    Added : added rule 1

--- src/lambdaexpression/UserRegistration.java
+++ src/lambdaexpression/UserRegistration.java
@@ -3,36 +3,12 @@ import java.util.regex.*;
 
 public class UserRegistration {
 
-	 private static final String FIRST_NAME_PATTERN = "^[A-Z]{1}[a-z]{2,}$";
-	  private static final String LAST_NAME_PATTERN = "^[A-Z]{1}[a-z]{2,}$";
-	  private static final String EMAIL_ID_PATTERN = "^[a-zA-Z0-9_-]+(?:\\.[a-zA-Z0-9_-]+)*@[a-zA-Z0-9_-]+\\.[a-zA-Z0-9_-]+(?:\\.[a-zA-Z0-9_-]+)*$";
-	  private static final String PHONE_NUMBER = "^[9][1]{0,1}\\s[0-9]{10}";
-	  
-	  public boolean validateFirstName (String fname){
-	         
-	            Pattern pattern = Pattern.compile(FIRST_NAME_PATTERN);
-	            return pattern.matcher(fname).matches();
-		  
-	                }
-	      
-	  public boolean validateLastName (String lname){
-	      
-	      Pattern pattern = Pattern.compile(LAST_NAME_PATTERN);
-	      return pattern.matcher(lname).matches();
-
-	  }
-	      public boolean validateEmailId (String emailid){
-	          
-	          Pattern pattern = Pattern.compile(EMAIL_ID_PATTERN);
-	          return pattern.matcher(emailid).matches();
-	          }
-	      
-	      public boolean validatePhoneNumber (String pname){
-	          
-	          Pattern pattern = Pattern.compile(PHONE_NUMBER);
-	          return pattern.matcher(pname).matches();
-
-		                }
-	
+ private static final String PASSWORD_RULE1 = "^[a-zA-Z0-9_-]{8,}$";
+	 
+	 public boolean validatepassword_rule1 (String pwd_rule1){
+         
+         Pattern pattern = Pattern.compile(PASSWORD_RULE1);
+         return pattern.matcher(pwd_rule1).matches();  
+	 }              
 
 }
