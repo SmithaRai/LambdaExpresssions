5f49c9739c285ffabe8e3d6cd545deaa0da201fc
unknown <SmithaRai.com>
2022-04-23 20:31:43 +0530

    Added : added regex for emailid

--- src/lambdaexpression/UserRegistration.java
+++ src/lambdaexpression/UserRegistration.java
@@ -3,8 +3,9 @@ import java.util.regex.*;
 
 public class UserRegistration {
 
-	private static final String FIRST_NAME_PATTERN = "^[A-Z]{1}[a-z]{2,}$";
+	 private static final String FIRST_NAME_PATTERN = "^[A-Z]{1}[a-z]{2,}$";
 	  private static final String LAST_NAME_PATTERN = "^[A-Z]{1}[a-z]{2,}$";
+	  private static final String EMAIL_ID_PATTERN = "^[a-zA-Z0-9_-]+(?:\\.[a-zA-Z0-9_-]+)*@[a-zA-Z0-9_-]+\\.[a-zA-Z0-9_-]+(?:\\.[a-zA-Z0-9_-]+)*$";
 	  
 	  public boolean validateFirstName (String fname){
 	         
@@ -18,6 +19,11 @@ public class UserRegistration {
 	      Pattern pattern = Pattern.compile(LAST_NAME_PATTERN);
 	      return pattern.matcher(lname).matches();
 
+	  }
+	      public boolean validateEmailId (String emailid){
+	          
+	          Pattern pattern = Pattern.compile(EMAIL_ID_PATTERN);
+	          return pattern.matcher(emailid).matches();
 		                }
 	
 
