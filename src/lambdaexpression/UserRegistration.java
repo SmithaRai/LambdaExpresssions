6df5b14fefa1fa6382535200506d168627ead570
unknown <SmithaRai.com>
2022-04-23 20:46:56 +0530

    Added : added rule 2

--- src/lambdaexpression/UserRegistration.java
+++ src/lambdaexpression/UserRegistration.java
@@ -3,12 +3,21 @@ import java.util.regex.*;
 
 public class UserRegistration {
 
- private static final String PASSWORD_RULE1 = "^[a-zA-Z0-9_-]{8,}$";
+	 private static final String PASSWORD_RULE1 = "^[a-zA-Z0-9_-]{8,}$";
+	 private static final String PASSWORD_RULE2 = "^[a-zA-Z0-9_-]{8,}[A-Z]+$";
 	 
 	 public boolean validatepassword_rule1 (String pwd_rule1){
          
          Pattern pattern = Pattern.compile(PASSWORD_RULE1);
-         return pattern.matcher(pwd_rule1).matches();  
+         return pattern.matcher(pwd_rule1).matches();
+	  
+             }
+	 
+public boolean validatepassword_rule2 (String pwd_rule2){
+         
+         Pattern pattern = Pattern.compile(PASSWORD_RULE2);
+         return pattern.matcher(pwd_rule2).matches();
+	    
 	 }              
 
 }
